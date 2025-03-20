05e49296cd865fbc277b301910d00b7996450bbe
dali012 <dali.jerbi97@gmail.com>
2025-03-20 09:03:45 +0100

    fix requests number

--- src/utils/getAllRequest.ts
+++ src/utils/getAllRequest.ts
@@ -1,3 +1,5 @@
+import { calculateTotalRequests } from "./calculateSum";
+
 // Calculate timestamps for the last 24 hours
 const endTime = new Date().toISOString();
 const startTime = new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString();
@@ -57,11 +59,9 @@ export async function getWorkersAnalytics() {
 
     const data = await response.json();
 
-    // Extract and display total number of requests
-    const totalRequests =
-      data?.data?.viewer?.accounts?.[0]?.workersInvocationsAdaptive?.[0].sum
-        ?.requests || 0;
-    return totalRequests;
+    const requests = calculateTotalRequests(data);
+
+    return requests;
   } catch (error) {
     console.error("Error fetching Workers analytics:");
     console.error(error);
